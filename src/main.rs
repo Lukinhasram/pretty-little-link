e5d58a35fcaf8e5440b9144e50a7172f3101564e
Lucas Ramos de Oliveira <137449735+Lukinhasram@users.noreply.github.com>
2025-06-07 17:30:09 -0300

    Added initial dependencies and tracing setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
+
+    // Tracing configuration
+    tracing_subscriber::registry()
+        .with(EnvFilter::try_from_default_env().unwrap_or_else(|_| "link_shortener=debug, tower_http=debug, axum::rejection=trace".into()))
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    tracing::info!("tracing running!");
 }
