defc91e78e572c4e11887e6d82810503104f3d9e
Lucas Ramos de Oliveira <137449735+Lukinhasram@users.noreply.github.com>
2025-06-08 14:51:06 -0300

    Changed health_check_handler response to a json instead of just the status code

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 mod errors;
 
-use axum::http::StatusCode;
+use axum::Json;
+use serde_json::{json, Value};
 
 #[tokio::main]
 async fn main() {
     use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
     use axum::{routing::get, Router};
-    use axum::http::StatusCode;
 
     // Tracing setup
     tracing_subscriber::registry()
@@ -21,12 +21,14 @@ async fn main() {
     tracing::info!("Router configured.");
 
     // Running the server
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("localhost:3000").await.unwrap();
     tracing::info!("Listening on {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn health_check_handler() -> StatusCode {
+async fn health_check_handler() -> Json<Value> {
     tracing::info!("Health check endpoint was called.");
-    StatusCode::OK
+    Json(json!({
+        "status": "OK"
+    }))
 }
