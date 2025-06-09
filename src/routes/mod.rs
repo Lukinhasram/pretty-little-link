847ed2faf37e1a56a65ca53b735da93a1ac48c47
Lucas Ramos de Oliveira <137449735+Lukinhasram@users.noreply.github.com>
2025-06-09 15:41:25 -0300

    fix: change URL parameter syntax
    Change :short_code for {short_code}

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -11,6 +11,6 @@ pub fn app_router(app_state: AppState) -> Router {
     Router::new()
         .route("/health", get(health::health_check_handler))
         .route("/shorten", post(links::create_short_link_handler))
-        .route("/:short_code", get(links::redirect_handler))
+        .route("/{short_code}", get(links::redirect_handler))
         .with_state(app_state)
 }
