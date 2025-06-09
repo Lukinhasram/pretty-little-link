edc9528ebe1d29b10c4a89febbf8e719e9897c44
Lucas Ramos de Oliveira <137449735+Lukinhasram@users.noreply.github.com>
2025-06-09 14:33:06 -0300

    Create models.rs

---
+++ src/models.rs
@@ -0,0 +1,20 @@
+use serde::Serialize;
+use serde::Deserialize;
+use sqlx::FromRow;
+
+#[derive(Deserialize)]
+pub struct CreateLinkRequest {
+    pub original_url: String,
+}
+
+#[derive(Serialize)]
+pub struct ShortLinkResponse {
+    pub short_url: String,
+}
+
+#[derive(Debug, FromRow)]
+pub struct Link {
+    pub id: i32,
+    pub short_code: String,
+    pub original_url: String,
+}
