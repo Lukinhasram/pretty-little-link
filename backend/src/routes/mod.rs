1297e3e3c8e1204b60eda8284ec8a0382ca583d3
Lucas Ramos de Oliveira <137449735+Lukinhasram@users.noreply.github.com>
2025-06-27 22:03:28 -0300

    fix: update cors handling for deploy on render

--- backend/src/routes/mod.rs
+++ backend/src/routes/mod.rs
@@ -7,13 +7,30 @@ use axum::{
     routing::{get, post},
 };
 use http::{Method};
+use std::env;
 use tower_http::cors::{Any, CorsLayer};
 
 pub fn app_router(app_state: AppState) -> Router {
-    let cors = CorsLayer::new()
-        .allow_origin(Any)
-        .allow_methods([Method::GET, Method::POST, Method::OPTIONS])
-        .allow_headers(Any);
+    let cors = match env::var("FRONTEND_URL") {
+        // If the FRONTEND_URL is set (in production)
+        Ok(frontend_url) => {
+            tracing::info!("CORS configured for production origin: {}", &frontend_url);
+            CorsLayer::new()
+                // ...allow requests only from that specific origin.
+                .allow_origin(frontend_url.parse::<http::HeaderValue>().unwrap())
+                .allow_methods([Method::GET, Method::POST, Method::OPTIONS])
+                .allow_headers(Any)
+        }
+        // If the FRONTEND_URL is NOT set (in local development)
+        Err(_) => {
+            tracing::info!("CORS configured for development (permissive)");
+            CorsLayer::new()
+                // ...allow requests from any origin.
+                .allow_origin(tower_http::cors::Any)
+                .allow_methods([Method::GET, Method::POST, Method::OPTIONS])
+                .allow_headers(Any)
+        }
+    };
 
     Router::new()
         .route("/health", get(health::health_check_handler))
